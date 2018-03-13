14acd51c75a01510923b34fd7e4879ab7e5fd118
Michael <michael.sargs1an@gmail.com>
2018-03-14 01:08:17 +0400

    add ping pong agues project and change lesson1 files

--- lesson1/michael/lessons1/scripts/scripts2.sh
+++ lesson1/michael/lessons1/scripts/scripts2.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 cd ../output/U1/
 echo "a.txt" > a.txt
-echo "U1:a.txt"
-cat a.txt
+echo -n "U1:a.txt:[ "
+cat a.txt | tr '\n' ' '
+echo "]"
 cd U2/U3/
 echo "a.txt" > a.txt
-echo "U3:a.txt"
-cat a.txt
+echo -n "U3:a.txt:[ "
+cat a.txt | tr '\n' ' '
+echo "]"
 echo "b.txt" > b.txt
-echo "U3:b.txt"
-cat b.txt
+echo -n "U3:b.txt:[ "
+cat b.txt | tr '\n' ' '
+echo "]"
--- lesson1/michael/lessons1/scripts/scripts3.sh
+++ lesson1/michael/lessons1/scripts/scripts3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash/
 cd ../output/U1
-rm a.txt
+rm -f a.txt
 cd U2/U3
 cp -a a.txt ../U4
 cp -a b.txt ../U4
-rm a.txt
-rm b.txt
+rm -f a.txt
+rm -f b.txt
